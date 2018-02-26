1f2d91bf10019fc06961faeae2b84c5d47792f96
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-02-26 17:42:47 +0400

    created ColorList Component

---
+++ source/components/ColorList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Color from './Color';
+
+import '../style/color-list.css';
+
+const ColorList = ({ className, colors = [], onRate = f => f, onRemove = f => f }) => (
+    <div className={(className) ? className + ' color-list' : 'color-list'}>
+        {colors.length === 0 ?
+            <p className="color-list__p">No Colors Listed. (Add a Color)</p> :
+            colors.map(color => (
+                <Color
+                    key={color.id}
+                    {...color}
+                    className="color-list__item"
+                    onRate={(rating) => onRate(color.id, rating)}
+                    onRemove={() => onRemove(color.id)}
+                />
+            ))
+        }
+    </div>
+);
+
+ColorList.propTypes = {
+    className: PropTypes.string,
+    color: PropTypes.array,
+    onRate: PropTypes.func,
+    onRemove: PropTypes.func,
+};
+
+export default ColorList
