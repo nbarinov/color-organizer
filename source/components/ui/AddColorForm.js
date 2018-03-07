34f9b8ff02fcdb74ba2af733a3c5f7fc3199568b
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-03-08 01:20:06 +0400

    fixed bugs in AddColorForm

--- source/components/ui/AddColorForm.js
+++ source/components/ui/AddColorForm.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import '../../style/add-color-form.css';
 
-const AddColorForm = ({ onNewColor = f => f }) => {
+const AddColorForm = ({ className, nNewColor = f => f }) => {
     let _title, _color;
 
     const submit = e => {
@@ -17,10 +17,13 @@ const AddColorForm = ({ onNewColor = f => f }) => {
     };
 
     return (
-        <form onSubmit={submit} className='app__add-color-form add-color-form'>
-            <input type="text" className="add-color-form__input" ref={input => _title = input} placeholder="Color title..." required />
-            <input type="color" className="add-color-form__color" ref={input => _color = input} required />
-            <button className="add-color-form__button">Add new color</button>
+        <form onSubmit={submit} className={(className) ? className + ' add-color-form' : 'add-color-form'}>
+            <h2 className='add-color-form__title'>Add Color</h2>
+            <div className="add-color-form__fields">
+                <input type="text" className="add-color-form__input" ref={input => _title = input} placeholder="Color title..." required />
+                <input type="color" className="add-color-form__color" ref={input => _color = input} required />
+                <button className="add-color-form__button">Add new color</button>
+            </div>
         </form>
     );
 };
