1c70346e80ab7f48fc744ff9e8b035108ff6b46a
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-03-10 19:52:52 +0400

    added new tests for Color Component

--- __tests__/components/ui/Color.test.js
+++ __tests__/components/ui/Color.test.js
@@ -1,16 +1,47 @@
 import Color from '../../../source/components/ui/Color';
 
-const { shallow } = Enzyme;
+const { mount, shallow } = Enzyme;
 
 describe("<Color /> UI Component", () => {
-    it("Renders correct properties", () => {
-        let output = shallow(
-            <Color title="Test Color"
-                   color="#f0f0f0"
-                   rating={3}
-                   timestamp="Mon Apr 11 2016 12:54:19 GMT-0700 (PDT)" />
-        ).html();
+    describe("Rendering for UI", () => {
+        it("Renders correct properties", () => {
+            let output = shallow(
+                <Color title="Test Color"
+                    color="#f0f0f0"
+                    rating={3}
+                    timestamp="Mon Apr 11 2016 12:54:19 GMT-0700 (PDT)" />
+            ).html();
 
-        expect(output).toMatchSnapshot();
+            expect(output).toMatchSnapshot();
+        });
+
+        it("renders parental className color", () =>
+            expect(
+                shallow(
+                    <Color title="Test Color"
+                        color="#f0f0f0"
+                        rating={3}
+                        timestamp="Mon Apr 11 2016 12:54:19 GMT-0700 (PDT)"
+                        className="parent__color" />
+                )
+                    .find('section.parent__color.color')
+                    .length
+            ).toBe(1)
+        );
+    });
+
+    describe("Functions", () => {
+        it("Invokes onRate property", () => {
+            mount(<Color title="Test Color" color="#F0F0F0" timestamp="Mon Apr 11 2016 12:54:19 GMT-0700 (PDT)" />)
+                .find('div.star-rating__stars')
+                .childAt(2)
+                .simulate('click')
+        })
+
+        it("Invokes onRate property", () => {
+            mount(<Color title="Test Color" color="#F0F0F0" timestamp="Mon Apr 11 2016 12:54:19 GMT-0700 (PDT)" />)
+                .find("button")
+                .simulate('click')
+        })
     });
 });
